f1fb8db7da40e6caea71085623f63d484a82d1ec
Felix Soubelet <felix.soubelet@protonmail.com>
2023-09-27 11:28:17 +0200

    add CLIC DR files (line json and madx sequence) as well as fixtures to easily have them in tests

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,21 +1,26 @@
 import pathlib
 
 import pytest
+import xtrack as xt
 
 from cpymad.madx import Madx
 from helpers import make_sps_thin, re_cycle_sequence
 
-# TODO: prepare paths to various files
+# ----- Paths to inputs ----- #
+
 CURRENT_DIR = pathlib.Path(__file__).parent
 INPUTS_DIR = CURRENT_DIR / "inputs"
 
-# The following can be kept (and .gitignored) locally but will be cloned (with tag) in the CI
-ACC_MODELS_SPS = INPUTS_DIR / "acc-models-sps"
+# Various necessary files in acc-models-sps
+ACC_MODELS_SPS = INPUTS_DIR / "acc-models-sps"  # .gitignored so can be kept locally, is cloned in our CI
 SPS_SEQUENCE = ACC_MODELS_SPS / "SPS_LS2_2020-05-26.seq"
 SPS_TOOLKIT = ACC_MODELS_SPS / "toolkit"
 SPS_LHC_IONS_OPTICS = ACC_MODELS_SPS / "strengths" / "lhc_ion.str"
 SPS_LHC_IONS_BEAMS = ACC_MODELS_SPS / "beams" / "beam_lhc_ion_injection.madx"
 
+# Files for CLIC Damping Ring used in Michail's scripts
+CLIC_DR_LINE_JSON = INPUTS_DIR / "chrom-corr_DR.newlattice_2GHz.json"
+CLIC_DR_SEQUENCE_MADX = INPUTS_DIR / "chrom-corr_DR.newlattice_2GHz.seq"
 
 # ----- Fixtures accessible to all tests ----- #
 
@@ -24,7 +29,7 @@ SPS_LHC_IONS_BEAMS = ACC_MODELS_SPS / "beams" / "beam_lhc_ion_injection.madx"
 def matched_sps_lhc_ions_injection() -> Madx:
     """
     A cpymad.Madx instance with loaded SPS sequence, lhc ions optics,
-    and matched parameters.
+    and matched parameters. It is a thin lattice.
     """
     with Madx(stdout=False) as madx:
         # Parameters for matching later on
@@ -55,6 +60,38 @@ def matched_sps_lhc_ions_injection() -> Madx:
         madx.command.vary(name="qpv_setvalue")
         madx.command.jacobian(calls=50, tolerance=1e-25)
         madx.command.endmatch()
+
+        # Yield, exits context manager only after the calling test is done
+        yield madx
+
+
+@pytest.fixture()
+def xsuite_line_CLIC_damping_ring() -> xt.Line:
+    """
+    A loaded xt.Line of the CLIC DR with chroma corrected, as used in
+    scripts from Michail to benchmark against.
+    """
+    return xt.Line.from_json(str(CLIC_DR_LINE_JSON.absolute()))
+
+
+@pytest.fixture()
+def madx_CLIC_damping_ring() -> Madx:
+    """
+    A cpymad.Madx instance with loaded CLIC DR sequence file and optics,
+    as used in scripts from Michail to benchmark against. It is a thin
+    lattice.
+    """
+    with Madx(stdout=False) as madx:
+        madx = Madx(stdout=False)
+        madx.call(str(CLIC_DR_SEQUENCE_MADX.absolute()))
+        # Makethin on RING sequence
+        n_slice_per_element = 4
+        madx.command.beam(particle="positron", energy=2.86, bunched=True)
+        madx.command.use(sequence="RING")
+        madx.command.select(flag="MAKETHIN", slice_=n_slice_per_element, thick=False)
+        madx.command.select(flag="MAKETHIN", pattern="wig", slice_=1)
+        madx.command.makethin(sequence="RING", makedipedge=True)
+        madx.command.use(sequence="RING")
 
         # Yield, exits context manager only after the calling test is done
         yield madx
