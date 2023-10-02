a3a9438369f5ddc61ef990903d540a37c4569556
Felix Soubelet <felix.soubelet@protonmail.com>
2023-10-02 15:17:20 +0200

    transfer of ion_BunchLength and renaming of arguments, plus starting docstring

--- xibs/formulary.py
+++ xibs/formulary.py
@@ -43,7 +43,7 @@ def bunch_length(
     particle_charge: int,
 ) -> float:
     """Analytical calculation for bunch length for protons / electrons.
-    
+
     This is a linear approximation which assumes that particles are in the center of the bucket only / mostly.
     ~~~ from Wiedermanns book ~~~
 
@@ -59,22 +59,59 @@ def bunch_length(
         energy_loss (float): ??? in [???]. TODO: check with Michail and in Wiedermann book.
         particle_charge (int): elementary particle charge, in # of Coulomb charges (for
             instance 1 for electron or proton).
-    
+
     Returns:
         The analytically calculated bunch length in [m].
     """
+    # fmt: off
     return (
         sigma_e
         * circumference
         * np.sqrt(
             abs(slip_factor)
             * total_energy_GeV
-            / (
-                2
-                * np.pi
-                * beta_rel
-                * harmonic_number
-                * np.sqrt(particle_charge**2 * rf_voltage**2 - energy_loss**2)
-            )
+            / (2 * np.pi * beta_rel * harmonic_number * np.sqrt(particle_charge**2 * rf_voltage**2 - energy_loss**2))
         )
     )
+    # fmt: on
+
+
+# This is ion_BunchLength from Michail's code, in general_functions.py
+# The arguments used to be Circumferance, Harmonic_Num, Energy_total, SlipF, Sigma_E, beta_rel, RF_Voltage, Z
+def ion_bunch_length(
+    circumference: float,
+    harmonic_number: int,
+    total_energy_GeV: float,
+    slip_factor: float,
+    sigma_e: float,
+    beta_rel: float,
+    rf_voltage: float,
+    particle_charge: int,
+):
+    """Analytical calculation for bunch length for ions.
+
+    This calculation does not work too well if the bucket is full. Implementation so far as I
+    can tell is as used in the scripts for LEIR for ions (some MAD-X script that LEIR studies were using for IBS).
+
+    TODO: figure out exactly the formula that was implemented here and reference / document it in the docstring.
+    Args:
+        circumference (float): machine circumference in [m].
+        harmonic_number (int): harmonic number of the RF system.
+        total_energy_GeV (float): total energy of the simulated particles in [GeV].
+        slip_factor (float): slip factor of the machine.
+        sigma_e (float): energy spread of the particles? TODO: check with Michail and in Wiedermann book.
+        beta_rel (float): relativistic beta of the simulated particles.
+        rf_voltage (float): RF voltage of the machine's cavities in [???]. TODO: check with Michail for the units.
+        particle_charge (int): elementary particle charge, in # of Coulomb charges (for
+            instance 1 for electron or proton).
+
+    Returns:
+        The analytically calculated bunch length in [m].
+    """
+    # fmt: off
+    return (
+        circumference
+        / (2.0 * np.pi * harmonic_number)
+        * np.arccos(1 - (sigma_e**2 * total_energy_GeV * abs(slip_factor) * harmonic_number * np.pi) / (beta_rel**2 * particle_charge * rf_voltage))
+    )
+    # fmt: on
