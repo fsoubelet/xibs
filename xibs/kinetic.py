0930d0d026c9e9d2ee341c32193c2844ee4a857f
Felix Soubelet <felix.soubelet@protonmail.com>
2023-10-03 17:32:46 +0200

    change a bit documentation section names

--- xibs/kinetic.py
+++ xibs/kinetic.py
@@ -1,8 +1,8 @@
 """
 .. _xibs-kinetic:
 
-Kinetic IBS Formalism
----------------------
+Kinetic Formalism
+-----------------
 
 Module with user-facing API to compute diffusion and friction terms from the Nagaitsev integrals according to the kinetic formalism, as well as the corresponding kinetic IBS kicks and apply them to particles.
 """
