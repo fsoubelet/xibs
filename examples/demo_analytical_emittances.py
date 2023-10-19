plot particles, fix calculation of new emittances step (was using bunch_length in the dt arg slot) and plot results cleanly

examples/demo_analytical_emittances.py
@@ -7,13 +7,14 @@ Analytical Growth Rates and Emittance Evolution
 ===============================================
 
 This example shows how to use the `~.xibs.analytical.NagaitsevIBS` class
-to calculate IBS growth rates and emittance evolution analytically.
+to calculate IBS growth rates and emittances evolutions analytically.
 
 We will demonstrate using an `xtrack.Line` of the ``CLIC`` damping ring,
 for a positron beam.
 """
-# sphinx_gallery_thumbnail_number = 1
+# sphinx_gallery_thumbnail_number = 2
 import json
+import warnings
 
 from dataclasses import dataclass
 
@@ -25,6 +26,8 @@ import xtrack as xt
 from xibs.analytical import NagaitsevIBS
 from xibs.formulary import bunch_length
 from xibs.inputs import BeamParameters, OpticsParameters
+
+warnings.filterwarnings("ignore")  # scipy integration routines might warn
 
 ###############################################################################
 # Let's start by defining the line and particle information, as well as some
@@ -41,7 +44,10 @@ nemitt_y = 3.7033e-09
 n_part = int(5e3)
 
 ###############################################################################
-# We can now set up the line as well as the particles:
+# Setting up line and particles
+# -----------------------------
+# Let's start by loading the `xtrack.Line`, activating RF cavities and 
+# generating a matched Gaussian bunch:
 
 line = xt.Line.from_json(line_file)
 line.build_tracker(extra_headers=["#define XTRACK_MULTIPOLE_NO_SYNRAD"])
@@ -66,6 +72,26 @@ particles = xp.generate_matched_gaussian_bunch(
 )
 
 ###############################################################################
+# To have a look at the generated particles (see the `xsuite user guide 
+# <https://xsuite.readthedocs.io/en/latest/particlesmanip.html>`_ for more)
+
+fig, (axx, axy, axz) = plt.subplots(3, 1, figsize=(8, 10))
+
+axx.plot(1e6 * particles.x, 1e5 * particles.px, ".", ms=2)
+axy.plot(1e6 * particles.y, 1e6 * particles.py, ".", ms=2)
+axz.plot(1e3 * particles.zeta, 1e3 * particles.delta, ".", ms=2)
+
+axx.set_xlabel(r"$x$ [$\mu$m]")
+axx.set_ylabel(r"$p_x$ [$10^{-5}$]")
+axy.set_xlabel(r"$y$ [$\mu$m]")
+axy.set_ylabel(r"$p_y$ [$10^{-3}$]")
+axz.set_xlabel(r"$z$ [$10^{-3}$]")
+axz.set_ylabel(r"$\delta$ [$10^{-3}$]")
+
+plt.tight_layout()
+plt.show()
+
+###############################################################################
 # We can compute initial (geometrical) emittances as well as the bunch length
 # from the `xpart.Particles` object:
 
@@ -79,9 +105,12 @@ geom_epsy = sig_y**2 / twiss["bety"][0]
 bunch_l = np.std(particles.zeta[particles.state > 0])
 
 ###############################################################################
-# Let us initiate the `~.xibs.analytical.NagaitsevIBS` class, from both the beam
-# parameters of the particles and the optics parameters of the line. For each of
-# these a specific `dataclass` is provided in the `~.xibs.inputs` module.
+# Computing Elliptic Integrals and IBS Growth Rates
+# -------------------------------------------------
+# Let us initiate the `~.xibs.analytical.NagaitsevIBS` class. It is fairly
+# simple and is done from both the beam parameters of the particles and the
+# optics parameters of the line. For each of these a specific `dataclass`
+# is provided in the `~.xibs.inputs` module.
 
 beam_params = BeamParameters(particles)
 optics = OpticsParameters(twiss)
@@ -90,46 +119,40 @@ IBS = NagaitsevIBS(beam_params, optics)
 ###############################################################################
 # As a first step, all calculations in rely on the computing of Nagaitsev integrals
 # :cite:p:`PRAB:Nagaitsev:IBS_formulas_fast_numerical_evaluation`, which is done with
-# a dedicated function:
+# a dedicated function. These are returned, but also stored internally in the **IBS**
+# object and will be updated internally each time they are computed.
 
 integrals = IBS.integrals(geom_epsx, geom_epsy, sig_delta)
-
-###############################################################################
-# These are returned, but also stored internally in the **IBS** object and will be
-# updated internally each time they are computed.
-
 print(integrals)
 print(IBS.elliptic_integrals)
 
 ###############################################################################
 # From these the **IBS** growth rates can be computed, which is again done by calling
 # a dedicated function. If the integrals mentioned above have not been computed yet,
-# an error will be raised for the user.
+# an error will be raised for the user. Once again, these are returned but also stored
+# internally and updated internally each time they are computed.
 
 growth_rates = IBS.growth_rates(geom_epsx, geom_epsy, sig_delta, bunch_l)
-
-###############################################################################
-# Once again, these are returned but also stored internally and updated internally
-# each time they are computed.
-
 print(growth_rates)
 print(IBS.ibs_growth_rates)
 
 ###############################################################################
-# Details on the returned values and their meaning can be found in the API
-# documentation of the relevant methods.
-#
+# Computing New Emittances from Growth Rates
+# ------------------------------------------
 # From these one can compute the emittances at the next time step. For the emittances
-# at the next turn, once should use :math:`\sfrac{1}{f_{rev}}` as the time step, which
+# at the next turn, once should use :math:`1 / f_{rev}` as the time step, which
 # is the default value used if it is not provided.
 
-new_geom_epsx, new_geom_epsy, new_sig_delta = IBS.emittance_evolution(geom_epsx, geom_epsy, sig_delta)
-
-print(new_geom_epsx)
-print(new_geom_epsy)
-print(new_sig_delta)
+new_geom_epsx, new_geom_epsy, new_sig_delta = IBS.emittance_evolution(
+    geom_epsx, geom_epsy, sig_delta
+)
+print(f"Next time step Epsilon_x = {new_geom_epsx}")
+print(f"Next time step Epsilon_y = {new_geom_epsy}")
+print(f"Next time step Sigma_delta = {new_sig_delta}")
 
 ###############################################################################
+# Analytical Evolution for Many Turns
+# -----------------------------------
 # One can then analytically look at the evolution through many turns by looping
 # over this calculation. Let's do this for 1000 turns. We will also re-compute
 # the Nagaitsev elliptic integrals and the IBS grwoth rates every 50 turns.
@@ -137,8 +160,7 @@ print(new_sig_delta)
 # slower the simulation as it is the most compute-intensive process.
 
 nturns = 1000  # number of turns to loop for
-ibs_step = 50  # turn frequency at which to re-compute the growth rates
-
+ibs_step = 50  # frequency at which to re-compute the growth rates in [turns]
 
 # Set up a dataclass to store the results
 @dataclass
@@ -161,8 +183,8 @@ turn_by_turn = Records(
 
 # Store the initial values
 turn_by_turn.bunch_length[0] = np.std(particles.zeta[particles.state > 0])
-turn_by_turn.sig_delta[0] = sig_delta
-turn_by_turn.epsilon_x[0] = (sig_x**2 - (twiss["dx"][0] * sig_delta) ** 2) / twiss["betx"][0]
+turn_by_turn.sig_delta[0] = new_sig_delta
+turn_by_turn.epsilon_x[0] = (sig_x**2 - (twiss["dx"][0] * new_sig_delta) ** 2) / twiss["betx"][0]
 turn_by_turn.epsilon_y[0] = sig_y**2 / twiss["bety"][0]
 
 # We loop here now
@@ -183,14 +205,16 @@ for turn in range(1, nturns):
             turn_by_turn.bunch_length[turn - 1],
         )
 
-    # ----- Compute the new emittances and bunch length ----- #
-    emit_x, emit_y, sig_m = IBS.emittance_evolution(
-        turn_by_turn.epsilon_x[turn - 1],
-        turn_by_turn.epsilon_y[turn - 1],
-        turn_by_turn.sig_delta[turn - 1],
-        turn_by_turn.bunch_length[turn - 1],
+    # ----- Compute the new emittances ----- #
+    new_emit_x, new_emit_y, new_sig_delta = IBS.emittance_evolution(
+        geom_epsx=turn_by_turn.epsilon_x[turn - 1],
+        geom_epsy=turn_by_turn.epsilon_y[turn - 1],
+        sigma_delta=turn_by_turn.sig_delta[turn - 1],
+        # dt = 1.0 / IBS.optics.revolution_frequency,  # default value
     )
-    sigma_e = sig_m * IBS.beam_parameters.beta_rel**2
+
+    # compute bunch length analytically as the Particles object hasn't changed
+    sigma_e = new_sig_delta * IBS.beam_parameters.beta_rel**2
     bunch_l = bunch_length(
         optics.circumference,
         harmonic_number,
@@ -205,39 +229,23 @@ for turn in range(1, nturns):
 
     # ----- Update the records with the new values ----- #
     turn_by_turn.bunch_length[turn] = bunch_l
-    turn_by_turn.sig_delta[turn] = sig_m
-    turn_by_turn.epsilon_x[turn] = emit_x
-    turn_by_turn.epsilon_y[turn] = emit_y
+    turn_by_turn.sig_delta[turn] = new_sig_delta
+    turn_by_turn.epsilon_x[turn] = new_emit_x
+    turn_by_turn.epsilon_y[turn] = new_emit_y
 
 ###############################################################################
-# After this is done running, we can plot the evolutions:
+# After this is done running, we can plot the evolutions across the turns:
 
-plt.rcParams.update(
-    {
-        "font.family": "serif",
-        "font.size": 20,
-        "axes.titlesize": 20,
-        "axes.labelsize": 20,
-        "xtick.labelsize": 20,
-        "ytick.labelsize": 20,
-        "legend.fontsize": 15,
-        "figure.titlesize": 20,
-    }
-)
+figure, (epsx, epsy, sigdelta) = plt.subplots(3, 1, sharex=True, figsize=(8, 10))
 
+epsx.plot(1e10 * turn_by_turn.epsilon_x)
+epsy.plot(1e13 * turn_by_turn.epsilon_y)
+sigdelta.plot(1e3 * turn_by_turn.sig_delta)
 
-figure, (epsx, epsy, sigdelta) = plt.subplots(1, 3, figsize=(16, 5))
-epsx.plot(turn_by_turn.epsilon_x)
 epsx.set_ylabel(r"$\varepsilon_x$ [$10^{-10}$m]")
-epsx.set_xlabel("Turns")
-
-epsy.plot(turn_by_turn.epsilon_y)
-epsy.set_ylabel(r"$\varepsilon_y$ [$10^{-10}$m]")
-epsy.set_xlabel("Turns")
-
-sigdelta.plot(turn_by_turn.sig_delta)
+epsy.set_ylabel(r"$\varepsilon_y$ [$10^{-13}$m]")
 sigdelta.set_ylabel(r"$\sigma_{\delta}$ [$10^{-3}$]")
-sigdelta.set_xlabel("Turns")
+sigdelta.set_xlabel("Turn Number")
 
 plt.tight_layout()
 plt.show()
@@ -251,3 +259,43 @@ plt.show()
 #
 #    - `~xibs.analytical`: `~.xibs.analytical.NagaitsevIBS`
 #    - `~xibs.inputs`: `~xibs.inputs.BeamParameters`, `~xibs.inputs.OpticsParameters`
+
+###############################################################################
+# TODO: remove below when this is resolved
+# I am comparing to the old code (xibs._old_michail) now and finding some (small)
+# differences. For instance:
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Ixx | NEW CODE, IBS.ibs_growth_rates.Tx |
+# | 356.725986354935  | 356.72598635530807  |
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Iyy | NEW CODE, IBS.ibs_growth_rates.Ty |
+# | 101.02642760866847 | 101.02642760877411 |
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Ipp | NEW CODE, IBS.ibs_growth_rates.Tz |
+# | 86.60401587714175 | 86.60401587723233 |
+# | ----------------- | ------------------ |
+# This may be due to the numba compiling of the iterative_RD method in the new
+# code, that in turn leads to a different numerical precision than the python
+# one in Michail's code? Literally same code but one is @jit()-ed
+# Worthy to note that when doing a np.isclose() comparison, the assertions
+# resolve to `True`. I will check without the jit() decorator and report below
+# 
+# Without the JIT decorator, the results are:
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Ixx | NEW CODE, IBS.ibs_growth_rates.Tx |
+# | 356.725986354935  | 356.72598635530807  |
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Iyy | NEW CODE, IBS.ibs_growth_rates.Ty |
+# | 101.02642760866847 | 101.02642760877411 |
+# | ----------------- | ------------------ |
+# | OLD CODE, IBS.Ipp | NEW CODE, IBS.ibs_growth_rates.Tz |
+# | 86.60401587714175 | 86.60401587723233 |
+# | ----------------- | ------------------ |
+# 
+# WE ARE LEARNING. Discrepancy is still here and still the same,
+# the numba JIT is not responsible for it.
+# 
+# 
+# OH MY GOD I WAS CALLING THE `IBS.emittance_evolution` function
+# with the wrong argument for `dt` because I copy-pasted from how
+# to call the old codes I'm benchmarking against. Like is good now