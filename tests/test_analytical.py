516780dd83f759d466b9b47e7994210c4da3f247
Felix Soubelet <felix.soubelet@protonmail.com>
2023-10-23 17:22:43 +0200

    remove a todo

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -19,11 +19,11 @@ def test_lhc_injection_protons_no_crossing(madx_lhc_injection_protons_no_crossin
     # --------------------------------------------------------------------
     # Get the growth rates from MAD-X
     # madx = madx_lhc_injection_protons_no_crossing  # fully set up from the config file
-    # madx.command.twiss()  # needs to be called before IBS! TODO: does it need centre=True?
+    # madx.command.twiss()  # needs to be called before IBS!
     # mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)  # careful about the factor 2 in MAD-X
     # --------------------------------------------------------------------
     # Load xsuite line
-    # line = xt.Line.from_json(TODO: path to json file)
+    # TODO: get from a fixture
     # p0 = xp.Particles(mass0=madx.beam.mass * 1e9, q0=madx.beam.charge, p0c=madx.beam.pc * 1e9)
     # line.particle_ref = p0
     # twiss = line.twiss(method="4d")
