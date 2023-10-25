50df209862823299406462740df38ee8bd7c1d3d
Felix Soubelet <felix.soubelet@protonmail.com>
2023-10-25 09:37:59 +0200

    fully write the first LHC test, which passes

--- tests/test_analytical.py
+++ tests/test_analytical.py
@@ -15,23 +15,36 @@ from xibs._old_michalis import MichalisIBS
 from xibs.analytical import NagaitsevIBS
 from xibs.inputs import BeamParameters, OpticsParameters
 
-
 def test_lhc_injection_protons(madx_lhc_injection_protons, xtrack_lhc_injection_protons):
     # --------------------------------------------------------------------
     # Get the growth rates from MAD-X
-    # madx = madx_lhc_injection_protons  # fully set up from the config file
-    # mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)  # careful about the factor 2 in MAD-X
+    madx, params = madx_lhc_injection_protons  # fully set up from the config file
+    mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)  # careful about the factor 2 in MAD-X
     # --------------------------------------------------------------------
     # Load equivalent xsuite line
-    # TODO: get from a fixture
-    # p0 = xp.Particles(mass0=madx.beam.mass * 1e9, q0=madx.beam.charge, p0c=madx.beam.pc * 1e9)
-    # line.particle_ref = p0
-    # twiss = line.twiss(method="4d")
-    # TODO: remember to override npart for the BeamParameters / MichalisIBS later on
+    line = xtrack_lhc_injection_protons
+    twiss = line.twiss(method="4d")
+    opticsparams = OpticsParameters(twiss)
+    beamparams = BeamParameters(line.particle_ref)
+    beamparams.n_part = madx.sequence.lhcb1.beam.npart  # as line.particle_ref is only 1 particle
     # --------------------------------------------------------------------
     # Get the growth rates from the old code (xibs._old_michalis)
+    MIBS = MichalisIBS()
+    MIBS.set_beam_parameters(line.particle_ref)
+    MIBS.set_optic_functions(twiss)
+    MIBS.Npart = beamparams.n_part  # also need to update this
+    MIBS.calculate_integrals(Emit_x=params.geom_epsx, Emit_y=params.geom_epsy, Sig_M=params.sig_delta, BunchL=params.bunch_length)
     # --------------------------------------------------------------------
     # Get the growth rates from the analytical module (xibs.analytical)
+    IBS = NagaitsevIBS(beamparams, opticsparams)
+    IBS.integrals(params.geom_epsx, params.geom_epsy, params.sig_delta)
+    IBS.growth_rates(params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length)
     # --------------------------------------------------------------------
-    # Compare the results and ensure they are consistent
-    pass
+    # Compare the results to old implementation
+    np.isclose(IBS.ibs_growth_rates.Tx, MIBS.Ixx)
+    np.isclose(IBS.ibs_growth_rates.Ty, MIBS.Iyy)
+    np.isclose(IBS.ibs_growth_rates.Tz, MIBS.Ipp)
+    # Compare the results to MAD-X (allow 10% deviation as MAD-X takes vertical disp into account)
+    np.isclose(IBS.ibs_growth_rates.Tx, mad_Tx, rtol=1e-2)
+    np.isclose(IBS.ibs_growth_rates.Ty, mad_Ty, rtol=1e-2)
+    np.isclose(IBS.ibs_growth_rates.Tz, mad_Tz, rtol=1e-2)
