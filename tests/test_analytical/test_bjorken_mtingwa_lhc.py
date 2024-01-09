1746fb5c0a5e973d576f09446234646037633e78
Felix Soubelet <felix.soubelet@protonmail.com>
2024-01-09 14:58:20 +0100

    adapt B&M tests to all also test with coasting beams

--- tests/test_analytical/test_bjorken_mtingwa_lhc.py
+++ tests/test_analytical/test_bjorken_mtingwa_lhc.py
@@ -7,6 +7,7 @@ FOR COMPARISONS, PLEASE KEEP IN MIND:
     #  - We have to explicitely set atol=0 as the numbers can be very small
 """
 import numpy as np
+import pytest
 
 from helpers import get_madx_ibs_growth_rates
 
@@ -16,10 +17,13 @@ from xibs.inputs import BeamParameters, OpticsParameters
 # ----- Tests at injection energy ----- #
 
 
-def test_lhc_injection_protons(madx_lhc_injection_protons):
+@pytest.mark.parametrize("bunched", [True, False])
+def test_lhc_injection_protons(madx_lhc_injection_protons, bunched):
     # --------------------------------------------------------------------
     # Get the growth rates from MAD-X
     madx, params = madx_lhc_injection_protons  # fully set up from the config file
+    madx.beam.bunched = bunched  # set bunched or coasting beam
+    madx.command.twiss(centre=True)  # re-iterate Twiss before calling IBS
     mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)
     # --------------------------------------------------------------------
     # Load the optics and beam params from MAD-X for maximum consistency
@@ -29,21 +33,26 @@ def test_lhc_injection_protons(madx_lhc_injection_protons):
     # --------------------------------------------------------------------
     # Compute the growth rates
     IBS = BjorkenMtingwaIBS(beamparams, opticsparams)
-    IBS.growth_rates(params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length)
+    IBS.growth_rates(
+        params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length, bunched=bunched
+    )
     # --------------------------------------------------------------------
     # Now we compare values to MAD-X
     assert np.isclose(IBS.ibs_growth_rates.Tx, mad_Tx, atol=0, rtol=1e-2)
-    assert np.isclose(IBS.ibs_growth_rates.Ty, mad_Ty, atol=0, rtol=1e-2)
+    assert np.isclose(IBS.ibs_growth_rates.Ty, mad_Ty, atol=0, rtol=2.5e-2)
     assert np.isclose(IBS.ibs_growth_rates.Tz, mad_Tz, atol=0, rtol=1e-2)
 
 
 # ----- Tests at top energy ----- #
 
 
-def test_lhc_top_protons(madx_lhc_top_protons):
+@pytest.mark.parametrize("bunched", [True, False])
+def test_lhc_top_protons(madx_lhc_top_protons, bunched):
     # --------------------------------------------------------------------
     # Get the growth rates from MAD-X
     madx, params = madx_lhc_top_protons  # fully set up from the config file
+    madx.beam.bunched = bunched  # set bunched or coasting beam
+    madx.command.twiss(centre=True)  # re-iterate Twiss before calling IBS
     mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)
     # --------------------------------------------------------------------
     # Load the optics and beam params from MAD-X for maximum consistency
@@ -53,7 +62,9 @@ def test_lhc_top_protons(madx_lhc_top_protons):
     # --------------------------------------------------------------------
     # Compute the growth rates
     IBS = BjorkenMtingwaIBS(beamparams, opticsparams)
-    IBS.growth_rates(params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length)
+    IBS.growth_rates(
+        params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length, bunched=bunched
+    )
     # --------------------------------------------------------------------
     # Now we compare values to MAD-X - need slightly higher margins
     assert np.isclose(IBS.ibs_growth_rates.Tx, mad_Tx, atol=0, rtol=2.5e-2)
@@ -61,11 +72,14 @@ def test_lhc_top_protons(madx_lhc_top_protons):
     assert np.isclose(IBS.ibs_growth_rates.Tz, mad_Tz, atol=0, rtol=1e-2)
 
 
-def test_lhc_top_ions(madx_lhc_top_ions):
+@pytest.mark.parametrize("bunched", [True, False])
+def test_lhc_top_ions(madx_lhc_top_ions, bunched):
     # --------------------------------------------------------------------
     # Get the growth rates from MAD-X
     madx, params = madx_lhc_top_ions  # fully set up from the config file
-    mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)  # careful about the factor 2 in MAD-X
+    madx.beam.bunched = bunched  # set bunched or coasting beam
+    madx.command.twiss(centre=True)  # re-iterate Twiss before calling IBS
+    mad_Tx, mad_Ty, mad_Tz = get_madx_ibs_growth_rates(madx)
     # --------------------------------------------------------------------
     # Load the optics and beam params from MAD-X for maximum consistency
     # (also, we want to use a centered Twiss to get closer to MAD-X results)
@@ -74,9 +88,11 @@ def test_lhc_top_ions(madx_lhc_top_ions):
     # --------------------------------------------------------------------
     # Compute the growth rates
     IBS = BjorkenMtingwaIBS(beamparams, opticsparams)
-    IBS.growth_rates(params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length)
+    IBS.growth_rates(
+        params.geom_epsx, params.geom_epsy, params.sig_delta, params.bunch_length, bunched=bunched
+    )
     # --------------------------------------------------------------------
     # Now we compare values to MAD-X
     assert np.isclose(IBS.ibs_growth_rates.Tx, mad_Tx, atol=0, rtol=1e-2)
-    assert np.isclose(IBS.ibs_growth_rates.Ty, mad_Ty, atol=0, rtol=1.6e-2)  # bit more margin in vertical
+    assert np.isclose(IBS.ibs_growth_rates.Ty, mad_Ty, atol=0, rtol=4.5e-2)  # bit more margin in vertical
     assert np.isclose(IBS.ibs_growth_rates.Tz, mad_Tz, atol=0, rtol=1e-2)
