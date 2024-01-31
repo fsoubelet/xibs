1592c9b608a43a6345bae444bbc0a51908f90628
Felix Soubelet <felix.soubelet@protonmail.com>
2024-01-31 15:52:44 +0100

    test of SR inclusion comparing to the values from Michalis

--- tests/test_analytical/test_emittances.py
+++ tests/test_analytical/test_emittances.py
@@ -4,9 +4,10 @@ The simple and fast case of the PS protons at injection is taken to compute the 
 """
 import numpy as np
 import xtrack as xt
-
+import xpart as xp
 from cpymad.madx import Madx
 
+from xibs._old_michalis import MichalisIBS
 from xibs.analytical import BjorkenMtingwaIBS, IBSGrowthRates, NagaitsevIBS, _SynchrotronRadiationInputs
 from xibs.inputs import BeamParameters, OpticsParameters
 
@@ -73,6 +74,160 @@ def test_emittance_evolution(madx_ps_injection_protons, xtrack_ps_injection_prot
     assert np.isclose(bl, 0.5685069199704035, atol=0)
 
 
+def test_emittance_evolution_with_synchrotron_radiation(xtrack_clic_damping_ring):
+    """Test emittance evolution with SR. Compare to Michalis results."""
+    # --------------------------------------------------------------------
+    # A whole setup to get initial values, same as in the CLIC DR benchmark
+    bunch_intensity = 4.4e9
+    sigma_z = 1.58e-3
+    nemitt_x = 5.6644e-07
+    nemitt_y = 3.7033e-09
+    n_part = int(5e3)
+    line = xtrack_clic_damping_ring
+    line.particle_ref = xp.Particles(mass0=xp.ELECTRON_MASS_EV, q0=1, p0c=2.86e9)
+    for cavity in [element for element in line.elements if isinstance(element, xt.Cavity)]:
+        cavity.lag = 180
+    twiss = line.twiss()
+    particles = xp.generate_matched_gaussian_bunch(
+        num_particles=n_part,
+        total_intensity_particles=bunch_intensity,
+        nemitt_x=nemitt_x,
+        nemitt_y=nemitt_y,
+        sigma_z=sigma_z,
+        line=line,
+    )
+    sig_x = np.std(particles.x[particles.state > 0])  # horizontal stdev
+    sig_y = np.std(particles.y[particles.state > 0])  # vertical stdev
+    sigma_delta = np.std(particles.delta[particles.state > 0])  # momentum spread
+    geom_epsx = (sig_x**2 - (twiss["dx"][0] * sigma_delta) ** 2) / twiss["betx"][0]
+    geom_epsy = (sig_y**2 - (twiss["dy"][0] * sigma_delta) ** 2) / twiss["bety"][0]
+    bunch_length = np.std(particles.zeta[particles.state > 0])
+    # --------------------------------------------------------------------
+    # Create input parameters and IBS classes
+    opticsparams = OpticsParameters(twiss)
+    beamparams = BeamParameters(particles)
+    NIBS = NagaitsevIBS(beamparams, opticsparams)
+    BMIBS = BjorkenMtingwaIBS(beamparams, opticsparams)
+    MIBS = MichalisIBS()
+    MIBS.set_beam_parameters(particles)
+    MIBS.set_optic_functions(twiss)
+    # --------------------------------------------------------------------
+    # Set hardcoded growth rates - faster test this way - we don't care about being
+    # realistic here, just that with these specific values the computation is correct
+    rates = IBSGrowthRates(Tx=1e-6, Ty=1e-6, Tz=1e-5)
+    NIBS.ibs_growth_rates = rates
+    BMIBS.ibs_growth_rates = rates
+    MIBS.Ixx = rates.Tx; MIBS.Iyy = rates.Ty; MIBS.Ipp = rates.Tz
+    # --------------------------------------------------------------------
+    # Get the SR inputs needed for the emittance evolution
+    sr_inputs = _get_sr_inputs_from_line(line)
+    # --------------------------------------------------------------------
+    # Compute ref emittances at next turn, aka using revolution time as time step
+    # We compare to expected values, for NagaitsevIBS and BjorkenMtingwaIBS
+    time_step = 1 / opticsparams.revolution_frequency
+    ref_epsx, ref_epsy, ref_sigma_delta = MIBS.emit_evol_with_SR(
+        geom_epsx, 
+        geom_epsy, 
+        sigma_delta, 
+        bunch_length, 
+        sr_inputs.equilibrium_epsx, 
+        sr_inputs.equilibrium_epsy, 
+        sr_inputs.equilibrium_sigma_delta, 
+        sr_inputs.tau_x, 
+        sr_inputs.tau_y, 
+        sr_inputs.tau_z, 
+        dt=time_step,
+    )
+    # --------------------------------------------------------------------
+    # Compute new emittances at next turn with same time step and compare
+    # First with NagaitsevIBS
+    epsx, epsy, sigd, bl = NIBS.emittance_evolution(
+        epsx=geom_epsx,
+        epsy=geom_epsy,
+        sigma_delta=sigma_delta,
+        bunch_length=bunch_length,
+        sr_equilibrium_epsx=sr_inputs.equilibrium_epsx,
+        sr_equilibrium_epsy=sr_inputs.equilibrium_epsy,
+        sr_equilibrium_sigma_delta=sr_inputs.equilibrium_sigma_delta,
+        sr_tau_x=sr_inputs.tau_x,
+        sr_tau_y=sr_inputs.tau_y,
+        sr_tau_z=sr_inputs.tau_z,
+        dt=time_step,
+    )
+    assert np.isclose(epsx, ref_epsx, atol=0)
+    assert np.isclose(epsy, ref_epsy, atol=0)
+    assert np.isclose(sigd, ref_sigma_delta, atol=0)
+    # --------------------------------------------------------------------
+    # And then with BjorkenMtingwaIBS
+    epsx, epsy, sigd, bl = BMIBS.emittance_evolution(
+        epsx=geom_epsx,
+        epsy=geom_epsy,
+        sigma_delta=sigma_delta,
+        bunch_length=bunch_length,
+        sr_equilibrium_epsx=sr_inputs.equilibrium_epsx,
+        sr_equilibrium_epsy=sr_inputs.equilibrium_epsy,
+        sr_equilibrium_sigma_delta=sr_inputs.equilibrium_sigma_delta,
+        sr_tau_x=sr_inputs.tau_x,
+        sr_tau_y=sr_inputs.tau_y,
+        sr_tau_z=sr_inputs.tau_z,
+        dt=time_step,
+    )
+    assert np.isclose(epsx, ref_epsx, atol=0)
+    assert np.isclose(epsy, ref_epsy, atol=0)
+    assert np.isclose(sigd, ref_sigma_delta, atol=0)
+    # --------------------------------------------------------------------
+    # We can do the same again with 1s as time step
+    ref_epsx, ref_epsy, ref_sigma_delta = MIBS.emit_evol_with_SR(
+        geom_epsx, 
+        geom_epsy, 
+        sigma_delta, 
+        bunch_length, 
+        sr_inputs.equilibrium_epsx, 
+        sr_inputs.equilibrium_epsy, 
+        sr_inputs.equilibrium_sigma_delta, 
+        sr_inputs.tau_x, 
+        sr_inputs.tau_y, 
+        sr_inputs.tau_z, 
+        dt=1,
+    )
+    # --------------------------------------------------------------------
+    # Compute new emittances at next turn with same time step and compare
+    # First with NagaitsevIBS
+    epsx, epsy, sigd, bl = NIBS.emittance_evolution(
+        epsx=geom_epsx,
+        epsy=geom_epsy,
+        sigma_delta=sigma_delta,
+        bunch_length=bunch_length,
+        sr_equilibrium_epsx=sr_inputs.equilibrium_epsx,
+        sr_equilibrium_epsy=sr_inputs.equilibrium_epsy,
+        sr_equilibrium_sigma_delta=sr_inputs.equilibrium_sigma_delta,
+        sr_tau_x=sr_inputs.tau_x,
+        sr_tau_y=sr_inputs.tau_y,
+        sr_tau_z=sr_inputs.tau_z,
+        dt=1,
+    )
+    assert np.isclose(epsx, ref_epsx, atol=0)
+    assert np.isclose(epsy, ref_epsy, atol=0)
+    assert np.isclose(sigd, ref_sigma_delta, atol=0)
+    # --------------------------------------------------------------------
+    # And then with BjorkenMtingwaIBS
+    epsx, epsy, sigd, bl = BMIBS.emittance_evolution(
+        epsx=geom_epsx,
+        epsy=geom_epsy,
+        sigma_delta=sigma_delta,
+        bunch_length=bunch_length,
+        sr_equilibrium_epsx=sr_inputs.equilibrium_epsx,
+        sr_equilibrium_epsy=sr_inputs.equilibrium_epsy,
+        sr_equilibrium_sigma_delta=sr_inputs.equilibrium_sigma_delta,
+        sr_tau_x=sr_inputs.tau_x,
+        sr_tau_y=sr_inputs.tau_y,
+        sr_tau_z=sr_inputs.tau_z,
+        dt=1,
+    )
+    assert np.isclose(epsx, ref_epsx, atol=0)
+    assert np.isclose(epsy, ref_epsy, atol=0)
+    assert np.isclose(sigd, ref_sigma_delta, atol=0)
+
 # ----- Helpers ----- #
 
 
@@ -90,7 +245,7 @@ def _get_sr_inputs_from_line(line: xt.Line) -> _SynchrotronRadiationInputs:
     sr_equilibrium_gepsy = twiss["eq_gemitt_y"]
 
     # We will need to store the equilibrium longitudinal emittance too for later
-    sr_eq_zeta = twiss["eq_gemitt_zeta"]  # or 'eq_gemitt_zeta' for geometric
+    sr_eq_zeta = twiss["eq_gemitt_zeta"]  # geometric
 
     # The equilibrium momentum spread is not directly provided but can be obtained via
     # a method of the twiss result, using the equilibrium emittances obtained above.
