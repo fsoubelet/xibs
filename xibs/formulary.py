730c7b950ece845b7cea588d6c973d30ebcce8a3
Felix Soubelet <felix.soubelet@protonmail.com>
2024-03-14 13:33:18 +0100

    add helper formulae

--- xibs/formulary.py
+++ xibs/formulary.py
@@ -10,6 +10,8 @@ from __future__ import annotations  # important for sphinx to alias ArrayLike
 
 import logging
 
+import numpy as np
+
 from numpy.typing import ArrayLike
 
 LOGGER = logging.getLogger(__name__)
@@ -31,3 +33,40 @@ def phi(beta: ArrayLike, alpha: ArrayLike, dx: ArrayLike, dpx: ArrayLike) -> Arr
         An array of phi values through the machine.
     """
     return dpx + alpha * dx / beta
+
+
+# ----- Some helpers ----- #
+
+
+def _bunch_length(particles: "xtrack.Particles") -> float:
+    """Get the bunch length from the particles."""
+    nplike = particles._context.nplike_lib
+    return nplike.std(particles.zeta[particles.state > 0])
+
+
+def _sigma_delta(particles: "xtrack.Particles") -> float:
+    """Get the standard deviation of the momentum spread from the particles."""
+    nplike = particles._context.nplike_lib
+    return nplike.std(particles.delta[particles.state > 0])
+
+
+def _geom_epsx(particles: "xtrack.Particles", betx: float, dx: float) -> float:
+    """
+    Horizontal geometric emittance at a location in the machine, for the beta
+    and dispersion functions at this location.
+    """
+    nplike = particles._context.nplike_lib
+    sigma_x = nplike.std(particles.x[particles.state > 0])
+    sig_delta = _sigma_delta(particles)
+    return (sigma_x**2 - (dx * sig_delta) ** 2) / betx
+
+
+def _geom_epsy(particles: "xtrack.Particles", bety: float, dy: float) -> float:
+    """
+    Vertical geometric emittance at a location in the machine, for the beta
+    and dispersion functions at this location.
+    """
+    nplike = particles._context.nplike_lib
+    sigma_y = nplike.std(particles.y[particles.state > 0])
+    sig_delta = _sigma_delta(particles)
+    return (sigma_y**2 - (dy * sig_delta) ** 2) / bety
