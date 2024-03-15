8c7215b8fe229c7b37bf2d47702ae0d42b19e100
Felix Soubelet <felix.soubelet@protonmail.com>
2024-03-15 17:02:31 +0100

    modernize

--- examples/benchmarks/sps_top_ions_simple_kicks.py
+++ examples/benchmarks/sps_top_ions_simple_kicks.py
@@ -1,5 +1,3 @@
-import logging
-import sys
 import warnings
 
 from dataclasses import dataclass
@@ -12,82 +10,66 @@ import xobjects as xo
 import xpart as xp
 import xtrack as xt
 
-from xibs._old_michalis import MichalisIBS
 from xibs.analytical import NagaitsevIBS
 from xibs.inputs import BeamParameters, OpticsParameters
+from xibs.formulary import _bunch_length, _geom_epsx, _geom_epsy, _sigma_delta
 from xibs.kicks import SimpleKickIBS
+from xibs._old_michalis import MichalisIBS
 
-logging.basicConfig(
-    level=logging.WARNING,
-    stream=sys.stdout,
-    format="[%(asctime)s] [%(levelname)s] - %(module)s.%(funcName)s:%(lineno)d - %(message)s",
-    datefmt="%H:%M:%S",
-)
-warnings.filterwarnings("ignore")  # scipy integration routines might warn
+warnings.filterwarnings("ignore")
 
-# ----- Helpers ----- #
-
-
-def _sigma_delta(parts: xp.Particles) -> float:
-    return np.std(parts.delta[parts.state > 0])
-
-
-def _bunch_length(parts: xp.Particles) -> float:
-    return np.std(parts.zeta[parts.state > 0])
-
-
-def _geom_epsx(parts: xp.Particles, twiss: xt.TwissTable) -> float:
-    sig_x = np.std(parts.x[parts.state > 0])
-    sig_delta = _sigma_delta(parts)
-    return (sig_x**2 - (twiss["dx"][0] * sig_delta) ** 2) / twiss["betx"][0]
-
-
-def _geom_epsy(parts: xp.Particles, twiss: xt.TwissTable) -> float:
-    sig_y = np.std(parts.y[parts.state > 0])
-    sig_delta = _sigma_delta(parts)
-    return (sig_y**2 - (twiss["dy"][0] * sig_delta) ** 2) / twiss["bety"][0]
-
-
-# ------------------- #
+# ----- Load line and build tracker ----- #
 
 context = xo.ContextCpu(omp_num_threads="auto")
-filepath = Path(__file__).parent.parent.parent / "tests" / "inputs" / "lines" / "sps_top_ions.json"
+xibs_repo = Path(__file__).absolute().parent.parent.parent
+filepath = xibs_repo / "tests" / "inputs" / "lines" / "sps_top_ions.json"
 line = xt.Line.from_json(filepath.absolute())
 line.build_tracker(context)
 line.optimize_for_tracking()
 twiss = line.twiss(method="4d")
 
-# Using fake values for beam parameters to be in a regime that 'stimulates' IBS
-bunch_intensity = int(3.5e11)  # from the test config
-sigma_z = 8e-2
-nemitt_x = 1.0e-6
-nemitt_y = 0.2e-6
-
-# Let's get our parameters
-beamparams = BeamParameters.from_line(line, n_part=bunch_intensity)
-opticsparams = OpticsParameters.from_line(line)
+# ----- Activate RF Systems ----- #
 
 rf_voltage = 1.7e6  # 1.7MV from the test config
 harmonic_number = 4653
 cavity = "actcse.31632"
 line[cavity].lag = 180  # 0 if below transition, 180 if above
 line[cavity].voltage = rf_voltage  # In Xsuite for ions, do not multiply by charge as in MADX
-line[cavity].frequency = opticsparams.revolution_frequency * harmonic_number
+line[cavity].frequency = OpticsParameters.from_line(line).revolution_frequency * harmonic_number
 
-# Re-create particles with less elements as tracking takes a while
-n_part = int(2e3)
+# ----- Beam and Simulation Parameters ----- #
+
+# Using fake values for beam parameters to be in a regime that 'stimulates' IBS
+bunch_intensity = int(3.5e11)
+sigma_z = 8e-2
+nemitt_x = 1.0e-6
+nemitt_y = 0.2e-6
+n_part = int(1e3)
+nturns = 1000  # number of turns to loop for
+ibs_step = 50  # frequency at which to re-compute coefficients in [turns]
+
+# ----- Create particles ----- #
+
 particles = xp.generate_matched_gaussian_bunch(
     num_particles=n_part,
     total_intensity_particles=bunch_intensity,
     nemitt_x=nemitt_x,
     nemitt_y=nemitt_y,
     sigma_z=sigma_z,
+    particle_ref=line.particle_ref,
     line=line,
 )
 particles2 = particles.copy()
 
+# ----- Compute initial (geometrical) emittances & bunch length all in [m] ----- #
 
-# Set up a dataclass to store the results
+sig_delta = _sigma_delta(particles)
+bunch_l = _bunch_length(particles)
+geom_epsx = _geom_epsx(particles, twiss.betx[0], twiss.dx[0])
+geom_epsy = _geom_epsy(particles, twiss.bety[0], twiss.dy[0])
+
+# ----- Dataclass to store results ----- #
+
 @dataclass
 class Records:
     epsilon_x: np.ndarray
@@ -96,13 +78,15 @@ class Records:
     bunch_length: np.ndarray
 
     def update_at_turn(self, turn: int, parts: xp.Particles, twiss: xt.TwissTable):
-        self.epsilon_x[turn] = _geom_epsx(parts, twiss)
-        self.epsilon_y[turn] = _geom_epsy(parts, twiss)
+        """Automatically update the records at given turn from the xtrack.Particles."""
+        self.epsilon_x[turn] = _geom_epsx(parts, twiss.betx[0], twiss.dx[0])
+        self.epsilon_y[turn] = _geom_epsy(parts, twiss.bety[0], twiss.dy[0])
         self.sigma_delta[turn] = _sigma_delta(parts)
         self.bunch_length[turn] = _bunch_length(parts)
 
     @classmethod
     def init_zeroes(cls, n_turns: int) -> Self:  # noqa: F821
+        """Initialize the dataclass with arrays of zeroes."""
         return cls(
             epsilon_x=np.zeros(n_turns, dtype=float),
             epsilon_y=np.zeros(n_turns, dtype=float),
@@ -110,64 +94,53 @@ class Records:
             bunch_length=np.zeros(n_turns, dtype=float),
         )
 
-
-nturns = 1_000  # number of turns to loop for
-ibs_step = 50  # frequency at which to re-compute the growth rates / kick coefficients in [turns]
-turns = np.arange(nturns, dtype=int)  # array of turns
-
-# Initialize the dataclasses
-my_tbt = Records.init_zeroes(nturns)
-michalis_tbt = Records.init_zeroes(nturns)
+# Initialize the dataclasses & store initial values
+kicked_tbt = Records.init_zeroes(nturns)
+old_tbt = Records.init_zeroes(nturns)
 analytical_tbt = Records.init_zeroes(nturns)
 
-# Store the initial values
-my_tbt.update_at_turn(0, particles, twiss)
-michalis_tbt.update_at_turn(0, particles2, twiss)
+kicked_tbt.update_at_turn(0, particles, twiss)
+old_tbt.update_at_turn(0, particles, twiss)
 analytical_tbt.update_at_turn(0, particles, twiss)
 
-# Re-initialize the IBS classes to be sure
+# ----- Initialize our IBS models (old and new) ----- #
+
 beamparams = BeamParameters.from_line(line, n_part=bunch_intensity)
 opticsparams = OpticsParameters.from_line(line)
+
 IBS = SimpleKickIBS(beamparams, opticsparams)
 NIBS = NagaitsevIBS(beamparams, opticsparams)
 MIBS = MichalisIBS()
-MIBS.set_beam_parameters(line.particle_ref)
-MIBS.Npart = bunch_intensity
+MIBS.set_beam_parameters(particles)
 MIBS.set_optic_functions(twiss)
 
+# ----- We loop here now ----- # 
 
-# We loop here now
 for turn in range(1, nturns):
     # ----- Potentially re-compute the ellitest_parts integrals and IBS growth rates ----- #
     if (turn % ibs_step == 0) or (turn == 1):
-        print(
-            "=" * 60 + "\n",
-            f"Turn {turn:d}: re-computing growth rates and kick coefficients\n",
-            "=" * 60,
-        )
-        # We compute from values at the previous turn
+        print(f"Turn {turn:d}: re-computing growth rates and kick coefficients")
+        # Compute kick coefficients from the particle distribution at this moment
         IBS.compute_kick_coefficients(particles)
         MIBS.calculate_simple_kick(particles2)
-        NIBS.growth_rates(  # recomputes integrals by default
+        NIBS.growth_rates(
             analytical_tbt.epsilon_x[turn - 1],
             analytical_tbt.epsilon_y[turn - 1],
             analytical_tbt.sigma_delta[turn - 1],
             analytical_tbt.bunch_length[turn - 1],
         )
     else:
-        print(f"===== Turn {turn:d} =====")
+        print(f"Turn {turn:d}")
 
-    # ----- Apply IBS Kick and Track Turn ----- #
+    # ----- Manually Apply IBS Kick and Track Turn ----- #
     IBS.apply_ibs_kick(particles)
     MIBS.apply_simple_kick(particles2)
     line.track(particles, num_turns=1)
     line.track(particles2, num_turns=1)
 
-    # ----- Compute Emittances from Particles State for my tracked particles & update records----- #
-    my_tbt.update_at_turn(turn, particles, twiss)
-
-    # ----- Compute Emittances from Particles State for Michalis' tracked particles & update records----- #
-    michalis_tbt.update_at_turn(turn, particles2, twiss)
+    # ----- Update records for tracked particles ----- #
+    kicked_tbt.update_at_turn(turn, particles, twiss)
+    old_tbt.update_at_turn(turn, particles2, twiss)
 
     # ----- Compute analytical Emittances from previous turn values & update records----- #
     ana_emit_x, ana_emit_y, ana_sig_delta, ana_bunch_length = NIBS.emittance_evolution(
@@ -184,19 +157,20 @@ for turn in range(1, nturns):
 
 # ----- Plotting ----- #
 
+turns = np.arange(nturns, dtype=int)  # array of turns
 fig, axs = plt.subplot_mosaic([["epsx", "epsy"], ["sigd", "bl"]], sharex=True, figsize=(15, 8))
 
 # Plot from my kicks and tracking
-axs["epsx"].plot(turns, my_tbt.epsilon_x * 1e8, lw=1.5, label="Me")
-axs["epsy"].plot(turns, my_tbt.epsilon_y * 1e8, lw=1.5, label="Me")
-axs["sigd"].plot(turns, my_tbt.sigma_delta * 1e3, lw=1.5, label="Me")
-axs["bl"].plot(turns, my_tbt.bunch_length * 1e2, lw=1.5, label="Me")
+axs["epsx"].plot(turns, kicked_tbt.epsilon_x * 1e8, lw=1.5, label="Xibs")
+axs["epsy"].plot(turns, kicked_tbt.epsilon_y * 1e8, lw=1.5, label="Xibs")
+axs["sigd"].plot(turns, kicked_tbt.sigma_delta * 1e3, lw=1.5, label="Xibs")
+axs["bl"].plot(turns, kicked_tbt.bunch_length * 1e2, lw=1.5, label="Xibs")
 
 # Plot from Michalis kicks and tracking
-axs["epsx"].plot(turns, michalis_tbt.epsilon_x * 1e8, "-", lw=0.65, label="Michalis")
-axs["epsy"].plot(turns, michalis_tbt.epsilon_y * 1e8, "-", lw=0.65, label="Michalis")
-axs["sigd"].plot(turns, michalis_tbt.sigma_delta * 1e3, "-", lw=0.65, label="Michalis")
-axs["bl"].plot(turns, michalis_tbt.bunch_length * 1e2, "-", lw=0.65, label="Michalis")
+axs["epsx"].plot(turns, old_tbt.epsilon_x * 1e8, "-", lw=0.65, label="Michalis")
+axs["epsy"].plot(turns, old_tbt.epsilon_y * 1e8, "-", lw=0.65, label="Michalis")
+axs["sigd"].plot(turns, old_tbt.sigma_delta * 1e3, "-", lw=0.65, label="Michalis")
+axs["bl"].plot(turns, old_tbt.bunch_length * 1e2, "-", lw=0.65, label="Michalis")
 
 # Plot from analytical values
 axs["epsx"].plot(turns, analytical_tbt.epsilon_x * 1e8, lw=1, label="Analytical")
