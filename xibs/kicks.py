f4e95ef91ba755746981a9f70b84c8e2981ca724
Felix Soubelet <felix.soubelet@protonmail.com>
2024-03-15 11:48:47 +0100

    ugly debug

--- xibs/kicks.py
+++ xibs/kicks.py
@@ -793,6 +793,7 @@ class KineticKickIBS(KickBasedIBS):
         sig_px_norm_ = context.nparray_from_context_array(sigma_px_normalized)  # on CPU
         sig_py_norm_ = context.nparray_from_context_array(sigma_py_normalized)  # on CPU
         sig_delta_ = context.nparray_from_context_array(sigma_delta)            # on CPU
+        print(sig_px_norm_, sig_py_norm_, sig_delta_)
         delta_px_diffusion: ArrayLike = sig_px_norm_ * np.sqrt(2 * dt * Dx) * RNG.normal(0, 1, _size) * np.sqrt(rho_t * factor)   # on CPU
         delta_py_diffusion: ArrayLike = sig_py_norm_ * np.sqrt(2 * dt * Dy) * RNG.normal(0, 1, _size) * np.sqrt(rho_t * factor)   # on CPU
         delta_delta_diffusion: ArrayLike = sig_delta_ * np.sqrt(2 * dt * Dz) * RNG.normal(0, 1, _size) * np.sqrt(rho_t * factor)  # on CPU
