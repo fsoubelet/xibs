0c5509d6adaafb077886c04e0d0c5d14a9df2914
Felix Soubelet <felix.soubelet@protonmail.com>
2024-03-21 09:30:11 +0100

    prepare to turn into_demo

--- examples/qq.py
+++ examples/qq.py
@@ -1,6 +1,22 @@
-# sphinx_gallery_thumbnail_number = 2
-# import logging
-# import sys
+"""
+
+.. _demo-analytical-auto-growth-rates:
+
+=============================================
+Automatic Analytical Growth Rates Computation
+=============================================
+
+This example shows how to use the the auto recomputing functionality for growth rates
+in the analytical classes. To follow this example please first have a look at the 
+:ref:`Analytical Nagaitsev example <demo-analytical-nagaitsev>` as well as the relevant
+:ref:`FAQ section <xibs-faq-auto-recompute-growth-rates>`.
+
+This script will showcase the functionality by following an identical flow to the 
+Nagaitsev example linked above and expanding on the relevant parts. Demonstration will
+be done using the SPS top protons line.
+"""
+# sphinx_gallery_thumbnail_number = 1
+import logging
 import warnings
 
 from dataclasses import dataclass
@@ -15,24 +31,53 @@ from xibs.analytical import NagaitsevIBS
 from xibs.formulary import _bunch_length, _geom_epsx, _geom_epsy, _sigma_delta, _percent_change
 from xibs.inputs import BeamParameters, OpticsParameters
 
-warnings.simplefilter("ignore")  # for this tutorial's clarity
-plt.rcParams.update({"savefig.dpi": 300})
+# warnings.simplefilter("ignore")  # for this tutorial's clarity
+logging.basicConfig(
+    level=logging.WARNING,
+    format="[%(asctime)s] [%(levelname)s] - %(module)s.%(funcName)s:%(lineno)d - %(message)s",
+    datefmt="%H:%M:%S",
+)
+# plt.rcParams.update({"savefig.dpi": 300})
+plt.rcParams.update(
+    {
+        "font.family": "serif",
+        "font.size": 20,
+        "axes.titlesize": 20,
+        "axes.labelsize": 20,
+        "xtick.labelsize": 20,
+        "ytick.labelsize": 20,
+        "legend.fontsize": 15,
+        "figure.titlesize": 20,
+    }
+)
 
 
-
-
+###############################################################################
+# Let's start by defining the line and particle information, as well as some
+# parameters for later use:
 
 line_file = "lines/sps_top_protons.json"
-line = xt.Line.from_json(line_file)
 
-bunch_intensity = 6e11
+# The beam parameters are very exaggerated to enhance IBS effects
+bunch_intensity = 6e11  # very pushed
 sigma_z = 12e-2  # very pushed
 nemitt_x = 1e-6  # very pushed
 nemitt_y = 1e-6  # very pushed
 n_part = int(5e3)
 
+###############################################################################
+# Setting up line and particles
+# -----------------------------
+# Let's start by loading the `xtrack.Line`, activating RF cavities and
+# generating a matched Gaussian bunch:
+
+line = xt.Line.from_json(line_file)
+line.build_tracker()
+line.optimize_for_tracking()
+twiss = line.twiss(method="4d")
 
 # ----- Power accelerating cavities ----- #
+
 rf_voltage = 4  # in MV
 rf_frequency = 200e6
 harmonic_number = 4653
@@ -42,9 +87,6 @@ line[cavity_name].voltage = rf_voltage * 1e6  # to be given in [V] here
 line[cavity_name].lag = cavity_lag
 line[cavity_name].frequency = rf_frequency
 
-line.build_tracker()
-line.optimize_for_tracking()
-twiss = line.twiss(method="4d")
 
 particles = xp.generate_matched_gaussian_bunch(
     num_particles=n_part,
@@ -57,63 +99,79 @@ particles = xp.generate_matched_gaussian_bunch(
 )
 
 
-fig, (axx, axy, axz) = plt.subplots(3, 1, figsize=(9, 11))
+# fig, (axx, axy, axz) = plt.subplots(3, 1, figsize=(9, 11))
 
-axx.plot(1e6 * particles.x, 1e5 * particles.px, ".", ms=3)
-axy.plot(1e6 * particles.y, 1e6 * particles.py, ".", ms=3)
-axz.plot(1e3 * particles.zeta, 1e3 * particles.delta, ".", ms=3)
+# axx.plot(1e6 * particles.x, 1e5 * particles.px, ".", ms=3)
+# axy.plot(1e6 * particles.y, 1e6 * particles.py, ".", ms=3)
+# axz.plot(1e3 * particles.zeta, 1e3 * particles.delta, ".", ms=3)
 
-axx.set_xlabel(r"$x$ [$\mu$m]")
-axx.set_ylabel(r"$p_x$ [$10^{-5}$]")
-axy.set_xlabel(r"$y$ [$\mu$m]")
-axy.set_ylabel(r"$p_y$ [$10^{-3}$]")
-axz.set_xlabel(r"$z$ [$10^{-3}$]")
-axz.set_ylabel(r"$\delta$ [$10^{-3}$]")
-fig.align_ylabels([axx, axy, axz])
-plt.tight_layout()
-plt.show()
+# axx.set_xlabel(r"$x$ [$\mu$m]")
+# axx.set_ylabel(r"$p_x$ [$10^{-5}$]")
+# axy.set_xlabel(r"$y$ [$\mu$m]")
+# axy.set_ylabel(r"$p_y$ [$10^{-3}$]")
+# axz.set_xlabel(r"$z$ [$10^{-3}$]")
+# axz.set_ylabel(r"$\delta$ [$10^{-3}$]")
+# fig.align_ylabels([axx, axy, axz])
+# plt.tight_layout()
+# plt.show()
 
+###############################################################################
+# We can compute initial (geometrical) emittances as well as the bunch length
+# from the `xtrack.Particles` object:
 
 geom_epsx = _geom_epsx(particles, twiss.betx[0], twiss.dx[0])
 geom_epsy = _geom_epsy(particles, twiss.bety[0], twiss.dy[0])
 bunch_l = _bunch_length(particles)
 sig_delta = _sigma_delta(particles)
 
+###############################################################################
+# Instantiate Analytical IBS Objects
+# -------------------------------------------------
+# Let us instantiate three instances of the `~.xibs.analytical.NagaitsevIBS` class,
+# where each will be used for a different updating behaviour of the growth rates.
+
 
 beam_params = BeamParameters(particles)
 optics = OpticsParameters(twiss)
-IBS = NagaitsevIBS(beam_params, optics)  # recomputes at a given frequency
-AUTO_IBS = NagaitsevIBS(beam_params, optics)  # only auto recomputes
-MIX_IBS = NagaitsevIBS(beam_params, optics)  # does both
+
+IBS = NagaitsevIBS(beam_params, optics)  # will recompute rates at a given frequency
+AUTO_IBS = NagaitsevIBS(beam_params, optics)  # will auto-recompute rates only
+MIX_IBS = NagaitsevIBS(beam_params, optics)  # will do both of the above
+
+###############################################################################
+# Let's get them growth rates and see the evolution of the transverse emittances,
+# :math:`\sigma_{\delta}` and bunch length. We can then have a look at the relative
+# change from one second to the next.
 
 IBS.growth_rates(geom_epsx, geom_epsy, sig_delta, bunch_l)
 AUTO_IBS.growth_rates(geom_epsx, geom_epsy, sig_delta, bunch_l)
 MIX_IBS.growth_rates(geom_epsx, geom_epsy, sig_delta, bunch_l)
 
-
-
-
-
-
-
-
 new_geom_epsx, new_geom_epsy, new_sig_delta, new_bunch_length = IBS.emittance_evolution(
     geom_epsx, geom_epsy, sig_delta, bunch_l
 )
 
-
-# TODO: Here I could check the percent change and have a little conclusion
-# on how to choose our threshold
+# Let's see the relative change
 print(f"Geom. epsx: {geom_epsx:.2e} -> {new_geom_epsx:.2e} | ({_percent_change(geom_epsx, new_geom_epsx):.2e}% change)")
 print(f"Geom. epsy: {geom_epsy:.2e} -> {new_geom_epsy:.2e} | ({_percent_change(geom_epsy, new_geom_epsy):.2e}% change)")
 print(f"Sigma delta: {sig_delta:.2e} -> {new_sig_delta:.2e} | ({_percent_change(sig_delta, new_sig_delta):.2e}% change)")
 print(f"Bunch length: {bunch_l:.2e} -> {new_bunch_length:.2e} | ({_percent_change(bunch_l, new_bunch_length):.2e}% change)")
 
+###############################################################################
+# Analytical Evolution for Many Turns
+# -----------------------------------
+# We will loop over time steps (seconds in this case) for 3 hours equivalent time,
+# and specify the auto-recomputing in these steps where relevant.
 
-
-nsecs = 5 * 3_600  # that's 5h
-ibs_step = 10 * 60  # re-compute rates every 10min
+nsecs = 3 * 3_600  # that's 3h
+ibs_step = 10 * 60  # fixed interval for rates recomputing
 seconds = np.linspace(0, nsecs, nsecs, dtype=int)
+
+# This is the threshold that would trigger the auto-recomping of the
+# growth rates. If any quantity would change by more than 0.02%, then
+# the rates are re-computed to be the most up-to-date, and the new
+# evolution of emittances is computed.
+AUTO_PERCENT = 2e-2  # 0.02% change threshold
 
 
 # Set up a dataclass to store the results
@@ -145,15 +203,14 @@ class Records:
 
 # Initialize the dataclass & store the initial values
 regular = Records.init_zeroes(nsecs)
-regular.update_at_turn(0, geom_epsx, geom_epsy, sig_delta, bunch_l)
-
 auto = Records.init_zeroes(nsecs)
-auto.update_at_turn(0, geom_epsx, geom_epsy, sig_delta, bunch_l)
-
 mix = Records.init_zeroes(nsecs)
+
+regular.update_at_turn(0, geom_epsx, geom_epsy, sig_delta, bunch_l)
+auto.update_at_turn(0, geom_epsx, geom_epsy, sig_delta, bunch_l)
 mix.update_at_turn(0, geom_epsx, geom_epsy, sig_delta, bunch_l)
 
-
+# These arrays we will use to see when the auto-recomputing kicks in
 auto_recomputes = np.zeros(nsecs, dtype=float)
 mix_recomputes = np.zeros(nsecs, dtype=float)
 fixed_recomputes = np.zeros(nsecs, dtype=float)
@@ -161,14 +218,15 @@ fixed_recomputes = np.zeros(nsecs, dtype=float)
 
 # ----- We loop here now ----- #
 
-AUTO_PERCENT = 1e-2  # 0.01% change as threshold for auto-recompute
 for sec in range(1, nsecs):
+    # This is not necessary, just for showcasing in this tutorial
     n_recomputes_for_auto = AUTO_IBS._number_of_growth_rates_computations
     n_recomputes_for_mix = MIX_IBS._number_of_growth_rates_computations
+
     # ----- Potentially re-compute the IBS growth rates ----- #
     if (sec % ibs_step == 0) or (sec == 1):
         print(f"At {sec}s: re-computing growth rates the regular way")
-        # We compute from values at the previous turn
+        # Both below always re-compute the rates every 'ibs_step' seconds
         IBS.growth_rates(
             regular.epsilon_x[sec - 1],
             regular.epsilon_y[sec - 1],
@@ -193,6 +251,8 @@ for sec in range(1, nsecs):
     regular.update_at_turn(sec, new_emit_x, new_emit_y, new_sig_delta, new_bunch_length)
 
     # ----- Potentially auto-update growth rates and compute new emittances ----- #
+    # The AUTO_IBS is given our threshold for auto-recomputing and will decide to update
+    # its growth rates itself if any of the quantities change by more than 0.01%.
     anew_emit_x, anew_emit_y, anew_sig_delta, anew_bunch_length = AUTO_IBS.emittance_evolution(
         epsx=auto.epsilon_x[sec - 1],
         epsy=auto.epsilon_y[sec - 1],
@@ -204,6 +264,7 @@ for sec in range(1, nsecs):
     auto.update_at_turn(sec, anew_emit_x, anew_emit_y, anew_sig_delta, anew_bunch_length)
 
     # ----- Potentially auto-update growth rates and compute new emittances ----- #
+    # The MIX_IBS will also adopt this behaviour
     mnew_emit_x, mnew_emit_y, mnew_sig_delta, mnew_bunch_length = MIX_IBS.emittance_evolution(
         epsx=mix.epsilon_x[sec - 1],
         epsy=mix.epsilon_y[sec - 1],
@@ -215,6 +276,7 @@ for sec in range(1, nsecs):
     mix.update_at_turn(sec, mnew_emit_x, mnew_emit_y, mnew_sig_delta, mnew_bunch_length)
 
     # ----- Check if the rates were auto-recomputed ----- #
+    # This is also not necessary, just for showcasing in this tutorial
     if AUTO_IBS._number_of_growth_rates_computations > n_recomputes_for_auto:
         print(f"At {sec}s - Auto re-computed growth rates")
         auto_recomputes[sec] = 1
@@ -223,10 +285,12 @@ for sec in range(1, nsecs):
         mix_recomputes[sec] = 1
 
 
+# Here we also aggregate the fixed recomputes
 for sec in seconds:
     if (sec % ibs_step == 0) or (sec == 1):
         fixed_recomputes[sec - 1] = 1
 
+# And these are simply 1D arrays of the seconds at which re-computing happened
 where_auto_recomputes = np.flatnonzero(auto_recomputes)
 where_mix_recomputes = np.flatnonzero(mix_recomputes)
 where_fixed_recomputes = np.flatnonzero(fixed_recomputes)
@@ -234,28 +298,33 @@ where_fixed_recomputes = np.flatnonzero(fixed_recomputes)
 
 print(f"Fixed re-computes: {IBS._number_of_growth_rates_computations}")
 print(f"Auto re-computes: {AUTO_IBS._number_of_growth_rates_computations}")
-print(
-    f"Mix re-computes: {MIX_IBS._number_of_growth_rates_computations}, "
-    f"(on its own: {MIX_IBS._number_of_growth_rates_computations - IBS._number_of_growth_rates_computations})"
-)
+print(f"Mix re-computes: {MIX_IBS._number_of_growth_rates_computations}")
+diff = MIX_IBS._number_of_growth_rates_computations - IBS._number_of_growth_rates_computations
+print(f"\tOn its own: {diff})")
 
-# ----- Plotting the results ----- #
+###############################################################################
+# Feel free to run this simulation with different parameters, but beware that the
+# lower the auto-recompute threshold, the more likely it is that growth rates will
+# be re-computed at every turn, which can be very lengthy. Let's first have a look
+# at the evolution of emittances over time:
 
-fig, axs = plt.subplot_mosaic([["epsx", "epsy"], ["sigd", "bl"]], sharex=True, figsize=(13, 7))
+fig, axs = plt.subplot_mosaic([["epsx", "epsy"], ["sigd", "bl"]], sharex=True, figsize=(13, 8.5))
 
-# Add vertical lines at recompute times (first so they show up in the background)
+# We will add vertical lines at the times where recomputing of the growth 
+# rates happened (do this first so they show up in the background)
 for axis in axs.values():
     for sec in where_auto_recomputes:
         axis.axvline(sec / 3600, color="C1", linestyle="-", alpha=0.002)
-    for sec in where_mix_recomputes:  # if auto recomputed this did too
+    for sec in where_mix_recomputes:
         axis.axvline(sec / 3600, color="C2", linestyle="-", alpha=0.002)
+    # And very visible ones for the fixed recomputes
     for sec in where_fixed_recomputes:
         axis.axvline(sec / 3600, color="gray", linestyle="--", lw=1, alpha=0.2)
 
-axs["epsx"].plot(seconds / 3600, 1e9 * regular.epsilon_x, lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["epsy"].plot(seconds / 3600, 1e9 * regular.epsilon_y, lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["sigd"].plot(seconds / 3600, 1e4 * regular.sig_delta, lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["bl"].plot(seconds / 3600, 1e2 * regular.bunch_length, lw=2, label=f"Fixed ({ibs_step / 60} mins)")
+axs["epsx"].plot(seconds / 3600, 1e9 * regular.epsilon_x, lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["epsy"].plot(seconds / 3600, 1e9 * regular.epsilon_y, lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["sigd"].plot(seconds / 3600, 1e4 * regular.sig_delta, lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["bl"].plot(seconds / 3600, 1e2 * regular.bunch_length, lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
 
 axs["epsx"].plot(seconds / 3600, 1e9 * auto.epsilon_x, lw=1.9, label=f"Auto ({AUTO_PERCENT:.0e}% change)")
 axs["epsy"].plot(seconds / 3600, 1e9 * auto.epsilon_y, lw=1.9, label=f"Auto ({AUTO_PERCENT:.0e}% change)")
@@ -285,16 +354,27 @@ for axis in axs.values():
 
 fig.align_ylabels((axs["epsx"], axs["sigd"]))
 fig.align_ylabels((axs["epsy"], axs["bl"]))
-fig.suptitle("Analytical Evolution of Emittances from IBS\nSPS Top Ions")
+fig.suptitle("Analytical Evolution of Emittances from IBS\nSPS Top Protons")
 
 plt.legend(title="Recompute Rates")
 plt.tight_layout()
 plt.show()
 
 
-# ----- Plotting the percent changes ----- #
+# TODO: write from here
+###############################################################################
+# Feel free to run this simulation with different parameters, but beware that the
+# lower the auto-recompute threshold, the more likely it is that growth rates will
+# be re-computed at every turn, which can be very lengthy. Let's first have a look
+# at the evolution of emittances over time:
 
-fig, axs = plt.subplot_mosaic([["epsx", "epsy"], ["sigd", "bl"]], sharex=True, figsize=(13, 7))
+
+
+
+
+
+
+fig, axs = plt.subplot_mosaic([["epsx", "epsy"], ["sigd", "bl"]], sharex=True, figsize=(13, 8.5))
 
 # Add vertical lines at recompute times (first so they show up in the background)
 for axis in axs.values():
@@ -305,10 +385,10 @@ for axis in axs.values():
     for sec in where_fixed_recomputes:
         axis.axvline(sec / 3600, color="gray", linestyle="--", lw=1, alpha=0.2)
 
-axs["epsx"].plot(seconds / 3600, 1e2 * pd.Series(regular.epsilon_x).pct_change(), lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["epsy"].plot(seconds / 3600, 1e2 * pd.Series(regular.epsilon_y).pct_change(), lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["sigd"].plot(seconds / 3600, 1e2 * pd.Series(regular.sig_delta).pct_change(), lw=2, label=f"Fixed ({ibs_step / 60} mins)")
-axs["bl"].plot(seconds / 3600, 1e2 * pd.Series(regular.bunch_length).pct_change(), lw=2, label=f"Fixed ({ibs_step / 60} mins)")
+axs["epsx"].plot(seconds / 3600, 1e2 * pd.Series(regular.epsilon_x).pct_change(), lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["epsy"].plot(seconds / 3600, 1e2 * pd.Series(regular.epsilon_y).pct_change(), lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["sigd"].plot(seconds / 3600, 1e2 * pd.Series(regular.sig_delta).pct_change(), lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
+axs["bl"].plot(seconds / 3600, 1e2 * pd.Series(regular.bunch_length).pct_change(), lw=2, label=f"Fixed ({int(ibs_step / 60)} mins)")
 
 axs["epsx"].plot(seconds / 3600, 1e2 * pd.Series(auto.epsilon_x).pct_change(), lw=2, label=f"Auto ({AUTO_PERCENT:.0e}% change)")
 axs["epsy"].plot(seconds / 3600, 1e2 * pd.Series(auto.epsilon_y).pct_change(), lw=2, label=f"Auto ({AUTO_PERCENT:.0e}% change)")
